8d027e5cee144fc8943ca8f8b53c3231ed184c5f
Animes <animesbarman101@gmail.com>
2025-08-14 13:47:29 +0600

    logical operator

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express=require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app=express()
 const PORT=3000;
 
+
+
+
+// JSON ডাটা পার্স করার জন্য
+app.use(bodyParser.json());
+
+// form-urlencoded ডাটা পার্স করার জন্য
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -43,10 +52,115 @@ app.get('/', (req,res)=>{
 
 
 
+// create 
+
+app.post('/students', async (req,res)=>{
+
+    try{
+
+         const {name,age}=req.body
+
+         const newStudents=new studentsModel({
+            name,
+            age
+         })
+
+
+        const studentsSave=  await newStudents.save();
+
+
+        if(studentsSave){
+
+            res.status(200).send({
+                message:'save successfull',
+                data:studentsSave
+
+            })  
+        
+        }else{
+
+            res.status(400).send({
+                message:'faild to save'
+            })
+        }
+
+    }catch(err){
+
+        res.status(400).send({message:err.message})
+    }
+
+})
+
+
+// get red
+
+app.get('/students', async(req,res)=>{
+
+
+    try{
+
+        // const products=await studentsModel.find().limit(3);
+        const products=await studentsModel.find({age:{$eq:22}})
+
+        if(products){
+
+            res.status(200).send(products);
+
+        }else{
+
+            res.status(404).send({
+                message:'student not found'
+            })
+        }
+
+
+
+    }catch(err){
+
+        res.status(200).send(err.message);
+    }
+
+
+
+})
+
+
+app.get('/students/:id', async(req,res)=>{
+
+    try{
+
+        const {id}=req.params;
+
+        // const products=await studentsModel.find().limit(3);
+        // const products=await studentsModel.findOne({_id:id}).select({name:1})
+
+
+        if(products){
+
+            res.status(200).send(products);
+
+        }else{
+
+            res.status(404).send({
+                message:'student not found'
+            })
+        }
+
+
+
+    }catch(err){
+
+        res.status(200).send(err.message);
+    }
+
+
+
+})
+
+
 app.listen(PORT, async ()=>{
 
     console.log(`server is running http://localhost:${PORT}`)
     await dbConnected();
-
 
 })
